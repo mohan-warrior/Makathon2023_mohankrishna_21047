fc2942458d31876f2fcdd2c1bd9b50f72f35ec75
mohan-warrior <99632496+mohan-warrior@users.noreply.github.com>
2023-02-27 14:27:05 +0530

    Add files via upload

---
+++ question1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n,m;
+    long output;
+    cin>>n>>m;
+    if (n!=0){
+        i=n-1;
+    }
+    if (m!=0){
+        j=m-1;
+    }
+    output=i+j;
+    cout<<output;
+}
